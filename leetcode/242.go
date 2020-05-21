3a418aa4933b50342ccd1cb5bfe7d4a289807102
dino.ma <dino_ma@163.com>
2020-05-21 20:34:14 +0800

    leetcode 242 for go

---
+++ leetcode/242.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func isAnagram(s string, t string) bool  {
+	sLen := len(s);
+	tLen := len(t);
+	if sLen != tLen{
+		 return false;
+	}
+	sArray := [26]int{};
+	tArray := [26]int{};
+	for _, v := range s{
+		sArray[v-'a']++;
+	}
+	for _, v := range t {
+		tArray[v-'a']++;
+	}
+
+	return sArray == tArray;
+}
+
+func main()  {
+	var s = "anagram";
+	var t ="nagaram";
+	fmt.Print(isAnagram(s, t));
+}

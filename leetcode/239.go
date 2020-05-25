9943ebbf9185f8a559c5389fd5ff8940e7cc99ca
dino.ma <dino_ma@163.com>
2020-05-25 20:02:23 +0800

    go fmt

--- leetcode/239.go
+++ leetcode/239.go
@@ -7,30 +7,30 @@ func maxSlidingWindow(nums []int, k int) []int {
 	if length < k || length == 0 {
 		return nil
 	}
-	result := make([]int, length - k + 1 );
-	queue := make([]int, 0, length);
-	for i :=0; i<length ;i++  {
+	result := make([]int, length-k+1)
+	queue := make([]int, 0, length)
+	for i := 0; i < length; i++ {
 		//队列不为空并且当前的值大于队列最左侧的值则只保留队列最末尾的值
-		for len(queue) !=0 && nums[i]  >= nums[queue[len(queue)-1]] {
+		for len(queue) != 0 && nums[i] >= nums[queue[len(queue)-1]] {
 			queue = queue[:len(queue)-1]
 		}
 		//队列从头入
-		queue = append(queue, i);
+		queue = append(queue, i)
 
 		//窗口滑动
-		if queue[0] == i - k {
+		if queue[0] == i-k {
 			queue = queue[1:]
 		}
 		//记录每个窗口的结果 i+1 - k 从第一个形成为窗口开始记录
 		if i+1-k >= 0 {
-			result[i+1 - k ] = nums[queue[0]];
+			result[i+1-k] = nums[queue[0]]
 		}
 	}
 
-	return result;
+	return result
 }
 
 func main() {
-	nums := []int{1,3,-1,-3,5,3,6,7}
+	nums := []int{1, 3, -1, -3, 5, 3, 6, 7}
 	fmt.Println(maxSlidingWindow(nums, 3))
 }

4ffeaf8556ca4462a3b2b6813b1a911448033f89
dino.ma <dino_ma@163.com>
2020-05-27 16:08:38 +0800

    change

--- leetcode/239.go
+++ leetcode/239.go
@@ -3,28 +3,29 @@ package main
 import "fmt"
 
 func maxSlidingWindow(nums []int, k int) []int {
-	length := len(nums)
-	if length < k || length == 0 {
+	numsLength := len(nums)
+	if numsLength == 0 || k <= 0 {
 		return nil
 	}
-	result := make([]int, length-k+1)
-	queue := make([]int, 0, length)
-	for i := 0; i < length; i++ {
-		//队列不为空并且当前的值大于队列最左侧的值则只保留队列最末尾的值
+	queue := make([]int, 0, numsLength)
+	result := make([]int, numsLength-k+1)
+	for i := 0; i < numsLength; i++ {
+
 		for len(queue) != 0 && nums[i] >= nums[queue[len(queue)-1]] {
 			queue = queue[:len(queue)-1]
 		}
-		//队列从头入
+
 		queue = append(queue, i)
 
-		//窗口滑动
 		if queue[0] == i-k {
 			queue = queue[1:]
 		}
-		//记录每个窗口的结果 i+1 - k 从第一个形成为窗口开始记录
-		if i+1-k >= 0 {
-			result[i+1-k] = nums[queue[0]]
+
+		resultKey := i + 1 - k
+		if resultKey >= 0 {
+			result[resultKey] = nums[queue[0]]
 		}
+
 	}
 
 	return result

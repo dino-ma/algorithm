426861a4d134f9d6c4e2c45ad5c5d7952a27336c
dino.ma <dino_ma@163.com>
2020-07-28 21:46:09 +0800

    移动0 计算好边界

--- leetcode/283.go
+++ leetcode/283.go
@@ -2,29 +2,30 @@ package main
 
 import "fmt"
 
-func moveZeroes(nums []int)  []int{
+func moveZeroes(nums []int) []int{
 	length := len(nums)
-	if length == 0 {
+	if length == 0{
 		return nums
 	}
 	left := 0
 
 	for i:=0; i<length; i++ {
-		if nums[i] != 0 {
-			nums[left] = nums[i]
-			if i != left {
-				nums[left] = 0
-			}
-			left++
+		if nums[i] == 0 {
+			continue
 		}
+		nums[left] = nums[i]
+		if left != i {
+			nums[i] = 0
+		}
+		left++
+
 	}
 
 	return nums
-
 }
 
 func main()  {
-
-	result := moveZeroes([]int{0,1,0,3,12})
-	fmt.Println(result)
+	nums := []int{0,1,0,3,12}
+	//nums := []int{1}
+	fmt.Println(moveZeroes(nums))
 }

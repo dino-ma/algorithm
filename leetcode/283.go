acf5750b20a19a4398dd3be15b42a95ad7756883
dino.ma <dino_ma@163.com>
2020-07-30 21:24:17 +0800

    移动0 变种

--- leetcode/283.go
+++ leetcode/283.go
@@ -24,8 +24,29 @@ func moveZeroes(nums []int) []int{
 	return nums
 }
 
+
+
+func moveZeroesEnd(nums []int) []int{
+	length := len(nums)
+	if length < 1 {
+		return nil
+	}
+
+	left := 0
+	for i:=length-1; i >= 0;i-- {
+		if nums[i] == 0 && i > left{
+			nums[i] = nums[left]
+			nums[left] = 0
+			left++
+		}
+	}
+	return nums
+
+}
+
 func main()  {
 	nums := []int{0,1,0,3,12}
 	//nums := []int{1}
 	fmt.Println(moveZeroes(nums))
+	fmt.Println(moveZeroesEnd(nums))
 }

96dbbf031a9e73a79ad3e73c8d136a7d48291c82
dino.ma <dino_ma@163.com>
2020-08-05 23:57:28 +0800

    傻孩子一定要记得，去重的时候只能跟已计算过的数进行去重不能乱动

--- leetcode/15.go
+++ leetcode/15.go
@@ -6,52 +6,47 @@ import (
 )
 
 func threeSum(nums []int) [][]int {
-	length := len(nums)
-	if length < 3 {
+	maxLength := len(nums)
+	if maxLength < 3 {
 		return nil
 	}
-	sort.Ints(nums)//升序做排序 -4 -1 0 123
-	fmt.Println(nums)
-	result := make([][]int, 0, 0)//构造返回数据 默认 LEN CAP为0 依靠APPEND做扩容
-	fmt.Println(length)
-	//length - 1为了i和right不相交
-	for i := 0; i < length-1; i++ {
-		left, right := i+1, length-1
-		//nums[i]> 0 代表 三数之和必大于0 ，或者 i 和 i+1大于0 则可以跳过本次循环
-		if nums[i] > 0 || (nums[i]+nums[left]) > 0 {
+	sort.Ints(nums)
+	result := make([][]int, 0, 0)
+	for i := 0; i< maxLength-1 ; i++ {
+		left ,right := i+1, maxLength-1
+		if nums[i]>0 || nums[i] + nums[left] > 0  {
 			continue
 		}
-		//寻找三个指针 的相邻的值如果相等则跳过 避免重复值
-		if i > 0 && nums[i] == nums[i-1] {
+
+		if i>0 && nums[i] == nums[i-1] {
 			continue
 		}
-		//left right 不相交
+
 		for left < right {
-			//如果left指针 已跳过首次循环 则去除左侧相邻的相同值
-			if left > i + 1 && nums[left] == nums[left-1] {
+			if left > i+1  && nums[left] == nums[left-1]{
 				left++
 				continue
-
 			}
-			//如果right指针 已跳过首次循环 则去除左侧相邻的相同值
-			if right < length - 2 && nums[right] == nums[right+1]{
+
+			if right < maxLength-2 && nums[right] == nums[right+1]{
 				right--
 				continue
 			}
 
-			sum := nums[i] + nums[left] + nums[right]
-			//常规移动指针
-			if sum == 0 {
+
+			x := nums[i] + nums[left] +nums[right]
+			if x == 0 {
 				result = append(result, []int{nums[i], nums[left], nums[right]})
 				left++
 				right--
-			}else if sum > 0 {
+			} else if x >0 {
 				right--
-			}else if sum < 0 {
+			} else if x < 0 {
 				left++
 			}
 		}
 	}
+
 
 	return result
 }
@@ -66,6 +61,6 @@ func threeSum(nums []int) [][]int {
 
 func main() {
 	//2分sort O(Nlogn) find
-	nums := []int{-1, 0, 1, 2, -1, -4}
+	nums := []int{-1,0,1,2,-1,-4}
 	fmt.Println(threeSum(nums))
 }
